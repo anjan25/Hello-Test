11292529cf9f3b600ccab1750126c5687fe45f13
anjan25 <mulay.anjana@gmail.com>
2018-08-27 21:16:31 +0530

    from eclips java sum

---
+++ SumNum.java
@@ -0,0 +1,18 @@
+package com.hello.example;
+
+import java.util.Scanner;
+
+public class SumNum {
+
+	public static void main(String[] args) {
+		   Scanner readme = new Scanner(System.in);
+		   System.out.println("Enter Two Numbers (Press Enter after each):");
+		   //two variables to hold numbers
+		   double n1, n2, n3;
+		   n1 = readme.nextDouble();
+		   n2 = readme.nextDouble();
+		   n3 = n1 + n2;
+		   System.out.println("Total = " + n3);
+		  }
+
+}
